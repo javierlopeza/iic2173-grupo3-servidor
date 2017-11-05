d5dfa748b46467e721d7997409f9860016b74eb1
Guillermo Figueroa <gafigueroa@uc.cl>
2017-11-05 15:41:33 -0300

    Cargar datos con paginación

--- routes/products.js
+++ routes/products.js
@@ -3,23 +3,53 @@ var request = require('request');
 var Product = require("../models/product");
 var mongoose = require('mongoose');
 
+var iterations = 3
 
-module.exports.load = function(){
-  request({url: 'http://arqss17.ing.puc.cl:3000/products', json: true}, function(err, res, json) {
-    if (err) {
-      throw err;
-    }
-    for (var i = json.length - 1; i >= 0; i--) {
-      json[i]["length"] = json[i].name.length;
-      delete json[i]["price"]
-      console.log(json[i]);
-      var newProduct = new Product({
-        id: json[i].id,
-        category: json[i].category,
-        name: json[i].name,
-        length: json[i].name.length
-      });
-      newProduct.save();
-    }
-  });
+function sleep(ms){
+    return new Promise(resolve=>{
+        setTimeout(resolve,ms)
+    })
+}
+
+function loadProducts(from, callback){
+  var results = [];
+
+  for (var counter = from; counter < from + iterations; counter++){
+    request({url: 'http://arqss17.ing.puc.cl:3000/productos?page='+ counter, json: true}, function(err, res, json) {
+      if (err) {
+        throw err;
+      }
+      for (var i = json.length - 1; i >= 0; i--) {
+        json[i]["length"] = json[i].name.length;
+        delete json[i]["price"]
+        console.log(json[i]);
+        var newProduct = new Product({
+          id: json[i].id,
+          category: json[i].category,
+          name: json[i].name,
+          length: json[i].name.length
+        });
+        newProduct.save();
+      }
+      results.push(json.length);
+      if (results.length == iterations){
+
+        if (results.includes(0)){
+          callback(false);
+        } else {
+          callback(true);
+        }
+      }
+    });
+  }
+}
+
+module.exports.load = async function(){
+  loadMore = true;
+  for (var counter = 1; loadMore == true; counter += iterations){
+    loadProducts(counter, function(result){
+      loadMore = result;
+    });
+    await sleep(20);
+  }
 };
